fad84cb2bdddfed0f522b9b2d37a7fb4d8b9433d
Marek Marciński <marcinski.mar@gmail.com>
2018-11-03 10:41:55 +0100

    initialization

---
+++ src/main/java/pl/sdaacademy/adressbook/AdressbookApplication.java
@@ -0,0 +1,12 @@
+package pl.sdaacademy.adressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdressbookApplication.class, args);
+    }
+}
