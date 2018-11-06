da71b92644f1ad842888d5a67090240af001cdea
Jola <jolas774@gmail.com>
2018-11-06 11:59:57 +0100

    person_in_progress

--- src/main/java/dto/PersonDto.java
+++ src/main/java/dto/PersonDto.java
@@ -1,20 +1,35 @@
-package dto;//wyswietlanie po str fronntu, ołaska str, nie jest to mapowanie 1do 1
+package dto;//wyswietlanie po str fronntu, płaska str, nie jest to mapowanie 1 do 1
 
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserDto {
 
     private Long id;
 
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     private String position;
+
+    @NotBlank
     private String businessEmail;
+
     private String privateEmail;
+
     private String privatePhone;
+
     private String businessPhone;
+
+    @NotBlank
     private AddressDto addressDto;
+
     //lista kontaktow
 
 }
